a58c110c29db37bc74751fe5833cc3b36dfdacc6
GabrieleGarofalo <gabri.garo@outlook.it>
2024-03-02 20:10:50 +0100

    Card Dinamiche con componenti e array

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
 //import Componente1 from "./Componente1";
 //import Componente2 from "./Componente2";
-//import Product from "./Product";
+import Product from "./Product";
 import 'bootstrap/dist/css/bootstrap.min.css';
-const img = "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f12079.jpg";
-
 
 const primaCard = {
   nome: "Tazza 50pz.",
   img:
   "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f12079.jpg",
    prezzo: 8,
-}
+   costoFinale: 10,
+  };
 
 const secondaCard = {
   nome: "caricatore 50pz.",
   img:
-  "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f12079.jpg",
+  "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f13733.jpg",
    prezzo: 20,
-}
+   costoFinale: 22
+};
 
 function App() {
   return (
     <div className="App">
-        <h2>La prima card</h2>
+        <h2>Card Dinamiche</h2>
         <section className="card-section">
-        <Product nome={primaCard.nome} prezzo={primaCard.prezzo} img={primaCard.img}/>
-        <Product nome={secondaCard.nome} prezzo={secondaCard.prezzo} img={secondaCard.img}/>
+        <Product 
+        nome={primaCard.nome} 
+        prezzo={primaCard.prezzo} 
+        img={primaCard.img}
+        costoFinale={primaCard.costoFinale}
+        />
+        <Product 
+        nome={secondaCard.nome} 
+        prezzo={secondaCard.prezzo} 
+        img={secondaCard.img}
+        costoFinale={secondaCard.costoFinale}
+        />
         </section>
          {/*<Componente1/>*/}
         {/*<Componente2/>*/}
@@ -35,25 +45,7 @@ function App() {
 }
 
 
-const Product = (props) => {
-  console.log(props);
-  const prezzo = '14'
-const alt = 'valore';
-  return (
-    <article>
-    <div className='card'>
-    <img src={img} alt={`${alt}`}/>
-    <h6>10euro Amazon + 20p</h6>
-    <hr/>
-    <p>2.16 euro</p>
-    <p>bamby000</p>
-    </div>
-    <p className='card-time'>RIAPRE PRESTO</p>
-    <button>{prezzo}</button>
-    
-    </article>
-    );
-  }
+
 
 
 
--- src/Product.js
+++ src/Product.js
@@ -1,24 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const prezzo = '14'
-const img = "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/f12079.jpg";
-
-const alt = 'valore';
-//const pStyle = {textTransform: 'uppercase'}
 const Product = (props) => {
-  console.log(props.nome);
+  console.log(props);
+
   return (
     <article>
     <div className='card'>
-    <img src={img} alt={`${alt}`}/>
-    <h6>10euro Amazon + 20p</h6>
+    <img src={props.img} alt={`${props.nome}`}/>
+    <h6>{props.nome}</h6>
     <hr/>
-    <p>2.16 euro</p>
+    <p>{props.prezzo}</p>
     <p>bamby000</p>
     </div>
     <p className='card-time'>RIAPRE PRESTO</p>
-    <button>{prezzo}</button>
+    <button>{props.costoFinale}</button>
     
     </article>
     );
