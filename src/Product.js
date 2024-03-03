b03726b1239af86630b1e1c447d993f0aecdd96c
GabrieleGarofalo <gabri.garo@outlook.it>
2024-03-03 13:30:57 +0100

    click hover javascrips

--- src/Product.js
+++ src/Product.js
@@ -3,11 +3,31 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Product = ({img,costoFinale,nome,prezzo,children}) => {
   // const{costoFinale, nome, prezzo, img} = props
+   const handleClick = () => {
+    console.log("mi hai premuto");
+   }
+
+   const paramsHandler = (prezzo) =>{
+    console.log(prezzo);
+   }
+
+   const eventHandler = (price) => (e) => {
+    console.log(e.target.innerText);
+    console.log(price);
+   }
+
+   function prezzoPieno(price) {
+    return function prova(e){
+      console.log(e.target.innerText);
+      console.log(price);
+    }
+   }
 
   return (
-    <article>
+    // <article onMouseOver={() => console.log(costoFinale)}>
+    <article >
     <div className='card'>
-    <img src={img} alt={`${nome}`}/>
+    <img src={img} alt={`${nome}`} onClick ={handleClick}/>
     <h6>{nome}</h6>
     <hr/>
     <p>{prezzo}</p>
@@ -15,7 +35,8 @@ const Product = ({img,costoFinale,nome,prezzo,children}) => {
     <p>{children}</p>
     </div>
     <p className='card-time'>RIAPRE PRESTO</p>
-    <button>{costoFinale}</button>
+    <button onClick={() => alert(`nome: ${nome}`)}>{costoFinale}</button>
+    <button onClick={prezzoPieno(prezzo)}>args</button>
     
     </article>
     );
