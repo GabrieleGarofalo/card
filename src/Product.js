d74ed8f81f38c907bbe94df4dd676da7e3bbc91a
GabrieleGarofalo <gabri.garo@outlook.it>
2024-03-03 11:32:02 +0100

    decostruzione oggetto e spread operator

--- src/Product.js
+++ src/Product.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-const Product = (props) => {
-  console.log(props);
+const Product = ({img,costoFinale,nome,prezzo}) => {
+  // const{costoFinale, nome, prezzo, img} = props
 
   return (
     <article>
     <div className='card'>
-    <img src={props.img} alt={`${props.nome}`}/>
-    <h6>{props.nome}</h6>
+    <img src={img} alt={`${nome}`}/>
+    <h6>{nome}</h6>
     <hr/>
-    <p>{props.prezzo}</p>
+    <p>{prezzo}</p>
     <p>bamby000</p>
     </div>
     <p className='card-time'>RIAPRE PRESTO</p>
-    <button>{props.costoFinale}</button>
+    <button>{costoFinale}</button>
     
     </article>
     );
